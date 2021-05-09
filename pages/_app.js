f8c3d35e82e5a5ac38ef8260970c4120f0b27895
Dawit Aschalew <66549279+dawitaschalew@users.noreply.github.com>
2021-05-09 11:57:35 +0300

    Add Init code

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import "../sass/main.scss";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Component {...pageProps} />
+  );
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import HomeScreen from "../lib/screens/home/HomeScreen";
+
+const Home = () => {
+
+  return (
+    <HomeScreen />
+  );
+    
+};
+
+export default Home;
